649c35587cbd75eef79b196a6e2fff3b464e6f2b
TroniQ89 <minetux@outlook.fr>
2016-07-03 21:52:15 +0200

    Added site config.
    Added site config.

---
+++ src/cfg/sitecfg.php
@@ -0,0 +1,33 @@
+<?php
+
+/* With this, you can change the HTML language.
+Exemple:
+French:
+# lang_sitecfg = "fr";
+Spanish:
+# lang_sitecfg = "es";
+English:
+# lang_sitecfg = "en";
+*/
+lang_sitecfg = "en";
+
+/* With this, you can change your site's description in search engines.
+Exemple:
+# description_sitecfg = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin finibus, tortor at commodo condimentum, arcu lorem fringilla felis, quis commodo diam ex et leo.";
+*/
+description_sitecfg = "My site description";
+
+/* With this, you can change your keywords in search engine. Separate them
+with commas.
+Exemple:
+# keywords_sitecfg = "foo, bar";
+*/
+keywords_sitecfg = "keyword, site, foo, bar, all, your, base, are, belong, to, us";
+
+/* Turn this to "true" if you want to show your name in search engines, set to "false" if you don't want.
+You must set author's name in each page. 
+*/
+sayauthor_sitecfg = "true";
+
+# Indev.
+?>
