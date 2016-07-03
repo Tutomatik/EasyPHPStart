36bfd697596d30ede9c546c48be6cecf16c7b270
TroniQ89 <minetux@outlook.fr>
2016-07-03 22:39:13 +0200

    Update main.php

--- src/main.php
+++ src/main.php
@@ -30,11 +30,11 @@ require('cfg/sitecfg.php');
 
 
 <meta name="description" content="<?php echo($description_pagecfg." ~ ".$description_sitecfg); ?>" />
-<meta name="keywords" content="<?php echo($keywords_pagecfg.", ".$keywords_sitecfg); ?>" />
+<meta name="keywords" content="<?php echo($keywords_sitecfg); ?>" />
 <?php if($sayauthor_sitecfg = "true") { ?>
-<meta name="author" content="<?php echo($author_pagecfg); ?>" />
+<meta name="author" content="<?php echo($author_sitecfg); ?>" />
 <?php } ?>
-<meta name="generator" content="<?php echo($gensoft_intercfg.$genver_interncfg); ?>" />
+<meta name="generator" content="<?php echo($gensoft_interncfg.$genver_interncfg); ?>" />
 
 <!-- indev -->
 </head>
