a1d9a9183e26e471f262d5564aa06edc9b005b48
TroniQ89 <minetux@outlook.fr>
2016-07-03 22:32:11 +0200

    Update sitecfg.php
    Sorry. I forgot to put "$" before vars, because it's been a while I developed in PHP.

--- src/cfg/sitecfg.php
+++ src/cfg/sitecfg.php
@@ -3,31 +3,31 @@
 /* With this, you can change the HTML language.
 Exemple:
 French:
-# lang_sitecfg = "fr";
+# $lang_sitecfg = "fr";
 Spanish:
-# lang_sitecfg = "es";
+# $lang_sitecfg = "es";
 English:
-# lang_sitecfg = "en";
+# $lang_sitecfg = "en";
 */
-lang_sitecfg = "en";
+$lang_sitecfg = "en";
 
 /* With this, you can change your site's description in search engines.
 Exemple:
-# description_sitecfg = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin finibus, tortor at commodo condimentum, arcu lorem fringilla felis, quis commodo diam ex et leo.";
+# $description_sitecfg = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin finibus, tortor at commodo condimentum, arcu lorem fringilla felis, quis commodo diam ex et leo.";
 */
-description_sitecfg = "My site description";
+$description_sitecfg = "My site description";
 
 /* With this, you can change your keywords in search engine. Separate them
 with commas.
 Exemple:
-# keywords_sitecfg = "foo, bar";
+# $keywords_sitecfg = "foo, bar";
 */
-keywords_sitecfg = "keyword, site, foo, bar, all, your, base, are, belong, to, us";
+$keywords_sitecfg = "keyword, site, foo, bar, all, your, base, are, belong, to, us";
 
 /* Turn this to "true" if you want to show your name in search engines, set to "false" if you don't want.
 You must set author's name in each page. 
 */
-sayauthor_sitecfg = "true";
+$sayauthor_sitecfg = "true";
 
 # Indev.
 ?>
