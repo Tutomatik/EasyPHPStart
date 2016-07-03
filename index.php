2f1d17b5f81dc3db2a73960e4226361b33f39166
TroniQ89 <minetux@outlook.fr>
2016-07-03 22:09:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // This is a page example !!
 
-include('src/main.php'); // Include EasyPHPStart
+require('src/main.php'); // Include EasyPHPStart
 author_pagecfg = "Tutomatik Tests";
 description_pagecfg = "This is just a random test of Tutomatik.";
 keywords_pagecfg = "testpage, example"; // Don't put a comma after the last keyword
