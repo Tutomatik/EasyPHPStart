de71148b60660a0cec4b5d0078b14ddbe0b58966
TroniQ89 <minetux@outlook.fr>
2016-07-03 22:00:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php 
 
-echo('Test de EasyPHPStart version 0.0.1_1');
+// This is a page example !!
 
-// Some random code here.
+include('src/main.php'); // Include EasyPHPStart
+author_pagecfg = "Tutomatik Tests";
+description_pagecfg = "This is just a random test of Tutomatik."
+keywords_pagecfg = "testpage, example" // Don't put a comma after the last keyword
+
 
 ?>
